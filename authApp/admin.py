d0af4115335cb214b8b4340beca9d7358d9d7ab4
Brandon S <bssanchezr7@gmail.com>
2021-10-15 20:18:12 -0500

    migración funcionando

--- authApp/admin.py
+++ authApp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models.user import User
-from .models.account import Account
+from .models.producto import Producto
 
 admin.site.register(User)
-admin.site.register(Account)
+admin.site.register(Producto)
 
 # Register your models here.
--- authApp/models/__init__.py
+++ authApp/models/__init__.py
@@ -1,3 +1,2 @@
-from .account import Account
 from .user import User
 from .producto import Producto
