7674384a46b782e261880ef799aaa46c0b8ff110
Brandon S <bssanchezr7@gmail.com>
2021-10-21 20:29:00 -0500

    cambio en max_length de producto

--- authApp/models/producto.py
+++ authApp/models/producto.py
@@ -4,5 +4,5 @@ from .user import User
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, related_name='producto_user', on_delete=models.CASCADE)
-    nombre = models.CharField(max_length = 15)
+    nombre = models.CharField(max_length = 50)
     precio = models.IntegerField()
